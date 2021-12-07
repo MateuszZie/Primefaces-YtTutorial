cae118d9e93ee7d92f2d768a797b1876857a43bf
Mateusz Ziebura <mateusz_860@op.pl>
2021-12-07 19:47:24 +0100

    Mega Menu example.

---
+++ src/main/java/pl/mz/domain/MegaMenu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.mz.domain;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+/**
+ *'
+ * @author Mateusz
+ */
+@Named
+@Getter
+@Setter
+@RequestScoped
+public class MegaMenu {
+    private String orientation = "horizontal";
+}
